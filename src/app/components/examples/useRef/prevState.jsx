1f95abf24bb3c19078bc84df2cc88e03038d1731
LastFornit <lastfornit@gmail.com>
2021-11-03 10:57:55 +0300

    Hooks 11

---
+++ src/app/components/examples/useRef/prevState.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef, useState } from "react";
+import CardWrapper from "../../common/Card";
+import Divider from "../../common/divider";
+import SmallTitle from "../../common/typografy/smallTitle";
+const PrevStateExample = () => {
+    const prevState = useRef(false);
+    const [otherState, setOtherState] = useState("false");
+    const toggleOtherState = () => {
+        setOtherState((prevState) =>
+            prevState === "false" ? "true" : "false"
+        );
+    };
+    useEffect(() => {
+        prevState.current = otherState;
+    }, [otherState]);
+    return (
+        <CardWrapper>
+            <SmallTitle>Предыдущее состояние</SmallTitle>
+            <Divider />
+            <p>prev state: {prevState.current}</p>
+            <p>current state: {otherState}</p>
+            <button className="btn btn-primary" onClick={toggleOtherState}>
+                Toggle other State
+            </button>
+        </CardWrapper>
+    );
+};
+
+export default PrevStateExample;
